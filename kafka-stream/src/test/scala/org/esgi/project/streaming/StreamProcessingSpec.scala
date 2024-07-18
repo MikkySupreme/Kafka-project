b72f031c56d6152fceb1cda7aa5e41c960b5189d
ChnMichel <chn.mikky@gmail.com>
2024-07-18 12:18:21 +0200

    Dev : Problem name
    Fix val name

--- kafka-stream/src/test/scala/org/esgi/project/streaming/StreamProcessingSpec.scala
+++ kafka-stream/src/test/scala/org/esgi/project/streaming/StreamProcessingSpec.scala
@@ -37,7 +37,7 @@ class StreamProcessingSpec extends AnyFunSuite with PlayJsonSupport {
         toSerializer[Views]
       )
 
-    val bestOfViewsStore: KeyValueStore[String, Long] =
+    val countViewsStore: KeyValueStore[String, Long] =
       topologyTestDriver
         .getKeyValueStore[String, Long](
           StreamProcessing.countViewsStoreName
@@ -47,9 +47,9 @@ class StreamProcessingSpec extends AnyFunSuite with PlayJsonSupport {
       views.map(view => new TestRecord(view.id, view)).asJava
     )
 
-    assert(bestOfViewsStore.get("1") == 2)
-    assert(bestOfViewsStore.get("2") == 4)
-    assert(bestOfViewsStore.get("3") == 2)
+    assert(countViewsStore.get("1") == 2)
+    assert(countViewsStore.get("2") == 4)
+    assert(countViewsStore.get("3") == 2)
 
   }
 
